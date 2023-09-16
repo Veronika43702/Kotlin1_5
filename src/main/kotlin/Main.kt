415ef29c7709cd2b53f3756945b1e1c15cfe83e4
Nika Chekulaeva <nika.702@mail.ru>
2023-09-16 19:06:38 +0300

    adding all properties

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,25 +2,74 @@ import javax.swing.text.StyledEditorKit.BoldAction
 
 data class Post(
     val id: Int,
-    val owner_id: Int,
-    val from_id: Int,
     val date: String,
-    val text: String,
-    val comments: Comments,
-    val likes: Likes,
+    val text: String?,
+    val reply_owner_id: Int = 0,
+    val reply_post_id: Int = 0,
+    val owner_id: Int = 0,
+    val from_id: Int = 0,
+    val created_by: Int = 0,
+    val friends_only: Boolean = false,
+    val comments: Comments? = Comments(),
+    val copyright: Copyright = Copyright(),
+    val likes: Likes = Likes(),
+    val reposts: Reposts = Reposts(),
+    val views: Views = Views(),
+    val post_type: String = "post",
+    val geo: Geo? = Geo(),
+    val signer_id: Int = 0,
+    val copy_history: Array<Post> = arrayOf(),
     val can_pin: Boolean = true,
     val can_delete: Boolean = true,
     val can_edit: Boolean = true,
+    val is_pinned: Boolean = false,
+    val marked_as_ads: Boolean = false,
+    val is_favourite: Boolean = false,
+    val donut: Donut = Donut(),
+    val postponed_id: Int = 0
 ) {
     data class Comments(
         val count: Int = 0,
         val can_post: Boolean = true
     )
 
+    data class Copyright(
+        val id: Int = 0,
+        val link: String = "http",
+        val name: String = "",
+        val type: String = ""
+    )
+
     data class Likes(
         val count: Int = 0,
-        val user_likes: Boolean = false
+        val user_likes: Boolean = false,
+        val can_like: Boolean = false,
+        val can_publish: Boolean = false
     )
+
+    data class Reposts(
+        val count: Int = 0,
+        val user_reposts: Boolean = false
+    )
+
+    data class Views(
+        val count: Int = 0,
+    )
+
+    data class Geo(
+        val type: String = "",
+        val coordinates: String = "",
+        val place: String = ""
+    )
+
+    data class Donut(
+        val is_donat: Boolean = false,
+        val paid_duration: Int = 0,
+        val can_publish_free_copy: Boolean = true,
+        val edit_mode: String = "All"
+    )
+
+
 
 }
 
@@ -63,25 +112,25 @@ object WallService {
 }
 
 fun main(args: Array<String>) {
-//    val comment1 = Post.Comments(1, true)
-//    val comment2 = Post.Comments(2, false)
-//    val post1 = Post(0, 2, 1, "16.09.23", "Hello", Post.Comments(), Post.Likes())
-//    val post2 = Post(0, 1, 2, "20.09.23", "Hello", comment1, Post.Likes(user_likes = true))
-//    val post3 = Post(0, 2, 2, "20.09.23", "Time for fun", comment2, Post.Likes())
-//
-//    // добавление постов
-//    println(WallService.add(post1))
-//    println(WallService.add(post2))
-//    println(WallService.add(post3))
-//
-//    // вывод полученного массива
-//    println("массив постов:")
-//    WallService.printPosts()
-//    // обновление постов
-//    val post4 = Post(2, 0, 1, "21.10.23", "Goodbye", comment2, Post.Likes())
-//    val post5 = Post(10, 0, 1, "10.10.23", "Goodbye", comment1, Post.Likes())
-//    println(WallService.update(post4))
-//    println(WallService.update(post5))
-//    // вывод обновленного массива
-//    WallService.printPosts()
+    val comment1 = Post.Comments(1, true)
+    val comment2 = Post.Comments(2, false)
+    val post1 = Post(0,  "16.09.23", "Hello")
+    val post2 = Post(0, "20.09.23", "Hello", comments = comment1, likes = Post.Likes(user_likes = true))
+    val post3 = Post(0, "20.09.23", "Time for fun")
+
+    // добавление постов
+    println(WallService.add(post1))
+    println(WallService.add(post2))
+    println(WallService.add(post3))
+
+    // вывод полученного массива
+    println("массив постов:")
+    WallService.printPosts()
+    // обновление постов
+    val post4 = Post(2, "21.10.23", "Goodbye", comments = comment2)
+    val post5 = Post(10, "10.10.23", "Goodbye")
+    println(WallService.update(post4))
+    println(WallService.update(post5))
+    // вывод обновленного массива
+    WallService.printPosts()
 }
--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -13,7 +13,7 @@ class WallServiceTest {
     @Test
     fun add(){
         val comment1 = Post.Comments(1, true)
-        val post1 = Post(0, 2, 1, "16.09.23", "Hello", comment1, Post.Likes())
+        val post1 = Post(0, "20.09.23", "Hello", comments = comment1, likes = Post.Likes(user_likes = true))
 
         assertTrue(WallService.add(post1).id != 0)
     }
@@ -22,15 +22,16 @@ class WallServiceTest {
     fun updateExistingPost() {
         val comment1 = Post.Comments(1, true)
         val comment2 = Post.Comments(2, false)
-        val post1 = Post(0, 2, 1, "16.09.23", "Hello", Post.Comments(), Post.Likes())
-        val post2 = Post(0, 1, 2, "20.09.23", "Hello", comment1, Post.Likes(user_likes = true))
-        val post3 = Post(0, 2, 2, "20.09.23", "Time for fun", comment2, Post.Likes())
+        val post1 = Post(0,  "16.09.23", "Hello")
+        val post2 = Post(0, "20.09.23", "Hello", comments = comment1, likes = Post.Likes(user_likes = true))
+        val post3 = Post(0, "20.09.23", "Time for fun")
+
 
         WallService.add(post1)
         WallService.add(post2)
         WallService.add(post3)
 
-        val post4 = Post(2, 0, 1, "21.10.23", "Goodbye", comment2, Post.Likes())
+        val post4 = Post(2, "21.10.23", "Goodbye", comments = comment2)
 
         val result = WallService.update(post4)
         assertTrue(result)
@@ -40,15 +41,16 @@ class WallServiceTest {
     fun updateMissingPost() {
         val comment1 = Post.Comments(1, true)
         val comment2 = Post.Comments(2, false)
-        val post1 = Post(0, 2, 1, "16.09.23", "Hello", comment1, Post.Likes())
-        val post2 = Post(0, 1, 2, "20.09.23", "Hello", comment1, Post.Likes(user_likes = true))
-        val post3 = Post(0, 2, 2, "20.09.23", "Time for fun", comment2, Post.Likes(user_likes = true))
+        val post1 = Post(0,  "16.09.23", "Hello")
+        val post2 = Post(0, "20.09.23", "Hello", comments = comment1, likes = Post.Likes(user_likes = true))
+        val post3 = Post(0, "20.09.23", "Time for fun")
+
 
         WallService.add(post1)
         WallService.add(post2)
         WallService.add(post3)
 
-        val post4 = Post(10, 0, 1, "21.10.23", "Goodbye", comment2, Post.Likes())
+        val post4 = Post(100, "21.10.23", "Goodbye", comments = comment2)
 
         val result = WallService.update(post4)
         assertFalse(result)
